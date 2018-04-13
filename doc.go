8f317e8607c4d7948aaa643579b6e3724e0a683d
Benjamin Borbe <bborbe@rocketnews.de>
2018-04-13 10:45:01 +0200

    add doc

---
+++ doc.go
@@ -0,0 +1,9 @@
+// Copyright (c) 2016, Benjamin Borbe <bborbe@rocketnews.de>.
+// All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+/*
+K8s-Deploy is a tool for automaticly deploy manifest files into the Kubernetes cluster.
+*/
+package k8s_deploy
