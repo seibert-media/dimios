1184774714b4cb95e873f33a9e178705ee8bfbef
Benjamin Borbe <bborbe@rocketnews.de>
2018-04-13 15:39:00 +0200

    use server resources

--- k8s/remote/provider.go
+++ k8s/remote/provider.go
@@ -1,8 +1,6 @@
 package remote_provider
 
 import (
-	"strings"
-
 	"github.com/golang/glog"
 	"github.com/pkg/errors"
 	"github.com/seibert-media/k8s-deploy/k8s"
@@ -25,42 +23,31 @@ func New(config *k8s_restclient.Config) k8s.Provider {
 
 func (p *provider) GetObjects(namespace k8s.Namespace) ([]k8s_runtime.Object, error) {
 	var result []k8s_runtime.Object
-
 	glog.V(4).Infof("get objects from k8s for namespace %s", namespace)
 	discoveryClient, err := k8s_discovery.NewDiscoveryClientForConfig(p.config)
 	if err != nil {
 		return nil, errors.Wrap(err, "creating k8s_discovery client failed")
 	}
 	dynamicClientPool := k8s_dynamic.NewDynamicClientPool(p.config)
-
-	apiGroupResources, err := k8s_discovery.GetAPIGroupResources(discoveryClient)
+	apiResourceList, err := discoveryClient.ServerResources()
 	if err != nil {
-		return nil, errors.Wrap(err, "get api group resouces failed")
+		return nil, errors.Wrap(err, "get server resouces failed")
 	}
-	for _, apiGroupResource := range apiGroupResources {
-		for _, groupVersion := range apiGroupResource.Group.Versions {
-			apiResourceList, err := discoveryClient.ServerResourcesForGroupVersion(groupVersion.GroupVersion)
-			if err != nil {
-				return nil, errors.Wrap(err, "get server resouces failed")
-			}
-			client, err := dynamicClientPool.ClientForGroupVersionKind(apiResourceList.GroupVersionKind())
+	for _, apiResource := range apiResourceList {
+		glog.V(4).Infof("apiresouce %v", apiResource)
+		for _, resource := range apiResource.APIResources {
+			client, err := dynamicClientPool.ClientForGroupVersionKind(apiResource.GroupVersionKind())
 			if err != nil {
 				return nil, errors.Wrap(err, "get client for group")
 			}
-			for _, resource := range apiResourceList.APIResources {
-				// TODO ignore resouces with slash
-				if strings.Index(resource.Name, "/") != -1 {
-					continue
-				}
-				ri := client.Resource(&resource, namespace.String())
-				object, err := ri.List(k8s_metav1.ListOptions{})
-				if err != nil {
-					glog.V(4).Infof("list %s failed: %s", resource.Name, err)
-					continue
-				}
-				glog.V(4).Infof("add object %v", object)
-				result = append(result, object)
+			ri := client.Resource(&resource, namespace.String())
+			object, err := ri.List(k8s_metav1.ListOptions{})
+			if err != nil {
+				glog.V(4).Infof("list %s failed: %s", resource.Name, err)
+				continue
 			}
+			glog.V(4).Infof("add object %v", object)
+			result = append(result, object)
 		}
 	}
 	glog.V(1).Infof("read api completed. found %d objects in namespace %s", len(result), namespace)
