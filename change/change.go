c11a2e8e68aa5b84b4ca9bd1b3bc4503e7f55460
Benjamin Borbe <bborbe@rocketnews.de>
2018-04-13 11:02:38 +0200

    add doc

--- change/change.go
+++ change/change.go
@@ -4,14 +4,16 @@ import (
 	"fmt"
 
 	"github.com/bborbe/k8s_deploy/k8s"
-	"k8s.io/apimachinery/pkg/runtime"
+	k8s_runtime "k8s.io/apimachinery/pkg/runtime"
 )
 
+// Contains the Kubernetes object and if it was deleted or not
 type Change struct {
 	Deleted bool
-	Object  runtime.Object
+	Object  k8s_runtime.Object
 }
 
+// String representation of the change.
 func (c *Change) String() string {
 	if c.Deleted {
 		return fmt.Sprintf("DELETE %s", k8s.ObjectToString(c.Object))
