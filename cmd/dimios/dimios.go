4aebcf1db62d34303e593f3b7448a6b7832a0b98
Benjamin Borbe <bborbe@rocketnews.de>
2018-04-27 13:13:55 +0200

    remove kubeconfig fallback

--- cmd/dimios/dimios.go
+++ cmd/dimios/dimios.go
@@ -9,14 +9,10 @@ import (
 	"flag"
 	"fmt"
 	"os"
-	"runtime"
-
-	"path/filepath"
-
 	"github.com/golang/glog"
 	"github.com/kolide/kit/version"
 	"github.com/seibert-media/dimios/manager"
-	"k8s.io/client-go/util/homedir"
+	"runtime"
 )
 
 var (
@@ -27,28 +23,21 @@ var (
 	teamvaultPassPtr       = flag.String("teamvault-pass", "", "teamvault password")
 	teamvaultConfigPathPtr = flag.String("teamvault-config", "", "teamvault config")
 	stagingPtr             = flag.Bool("staging", false, "staging status")
-	versionInfo            = flag.Bool("version", true, "show version info")
-	dbg                    = flag.Bool("debug", false, "enable debug mode")
-	sentryDsn              = flag.String("sentryDsn", "", "sentry dsn key")
+	versionInfo            = flag.Bool("version", false, "show version info")
+	kubeconfig             = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
 )
 
 func main() {
 	defer glog.Flush()
 	glog.CopyStandardLogTo("info")
+	flag.Parse()
+	runtime.GOMAXPROCS(runtime.NumCPU())
 
 	if *versionInfo {
 		fmt.Printf("-- //S/M Dimios --\n")
 		version.PrintFull()
+		os.Exit(0)
 	}
-
-	var kubeconfig *string
-	if home := homedir.HomeDir(); home != "" {
-		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
-	} else {
-		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
-	}
-	flag.Parse()
-	runtime.GOMAXPROCS(runtime.NumCPU())
 
 	m := &manager.Manager{
 		Namespaces:          *namespacesPtr,
