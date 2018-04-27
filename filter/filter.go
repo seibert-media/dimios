057b03b380abf1a73b4286eb3bb0c4bff8a0541d
mlorenz <lorenz.swt@gmail.com>
2018-04-27 12:39:28 +0200

    added test to test actutall filter

--- filter/filter.go
+++ filter/filter.go
@@ -1,7 +1,34 @@
 package filter
 
-import k8s_runtime "k8s.io/apimachinery/pkg/runtime"
+import (
+	k8s_runtime "k8s.io/apimachinery/pkg/runtime"
+	k8s_unstructured "k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+)
 
-func Filter(whitelist []string, k8sobjects []k8s_runtime.Object) []k8s_runtime.Object {
-	return k8sobjects
+func Filter(whitelist []string, k8sobjects []k8s_runtime.Object) ([]k8s_runtime.Object, error) {
+	var filteredk8sobjects = []k8s_runtime.Object{}
+
+	for _, object := range k8sobjects {
+		u, err := k8s_runtime.DefaultUnstructuredConverter.ToUnstructured(object)
+		if err != nil {
+			continue
+		}
+		obj := &k8s_unstructured.Unstructured{
+			Object: u,
+		}
+		if (contains(whitelist, obj.GetKind())) {
+			filteredk8sobjects = append(filteredk8sobjects, obj)
+		}
+	}
+	return filteredk8sobjects, nil
+}
+
+
+func contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
 }
--- filter/filter_test.go
+++ filter/filter_test.go
@@ -4,18 +4,32 @@ import (
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	k8s_runtime "k8s.io/apimachinery/pkg/runtime"
+	k8s_metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/api/apps/v1"
 )
 
 var _ = Describe("Filter", func() {
 	var (
 		Whitelist = []string{""}
-		K8sobjects = []k8s_runtime.Object{}
+		TestDeployment = v1.Deployment{
+			TypeMeta: k8s_metav1.TypeMeta{
+				APIVersion: "extensions/v1beta1",
+				Kind:       "Deployment",
+				},
+		}
+		K8sobjects = []k8s_runtime.Object{&TestDeployment}
 	)
 
 	Describe("Whitelistfilter", func() {
-		It("returns correct count of k8s objects", func() {
-			var k8sdeployobjects = Filter(Whitelist, K8sobjects)
+		It("returns correct count of k8s objects with empty whitelist", func() {
+			var k8sdeployobjects, _ = Filter(Whitelist, K8sobjects)
 			Expect(k8sdeployobjects).ToNot(BeNil())
+			Expect(len(k8sdeployobjects)).To(Equal(0))
+		})
+		It("returns correct count of k8s objects with whitelist deployment", func() {
+			Whitelist = append(Whitelist, "Deployment")
+			var k8sdeployobjects, _ = Filter(Whitelist, K8sobjects)
+			Expect(len(k8sdeployobjects)).To(Equal(1))
 		})
 	})
 })
