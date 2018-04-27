430bb97101aca8a4c773e946183c651aaf2a8ab7
mlorenz <lorenz.swt@gmail.com>
2018-04-27 11:34:44 +0200

    added filter test structur

---
+++ filter/filter_test.go
@@ -0,0 +1,19 @@
+package filter
+
+import (
+	. "github.com/onsi/ginkgo"
+)
+
+var _ = Describe("Filter", func() {
+	var (
+	)
+
+	BeforeEach(func() {
+	})
+
+	Describe("Whitelistfilter", func() {
+		It("returns correct count of k8s objects", func() {
+			Filter()
+		})
+	})
+})
